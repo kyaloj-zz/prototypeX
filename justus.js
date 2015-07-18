f7a1f26c9948f8abd8f500a567b322d6c59fe705
justus <jmbaluka@mfarm.co.ke>
2015-07-18 11:24:29 +0300

    done with trends prototype skeleton

--- justus.js
+++ justus.js
@@ -2,4 +2,16 @@
       $('.slider').slider({full_width: true});
        $('.parallax').parallax();
        $(".button-collapse").sideNav();
+       $('select').material_select();
+
+       $('.datepicker').pickadate({
+		    selectMonths: true, 
+		    selectYears: 15 
+		  });
+
+
+
+
     });
+
+ 
