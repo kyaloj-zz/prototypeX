eaa98b690f05f71917e5dc9de7da248aa5d59523
justus <jmbaluka@mfarm.co.ke>
2015-07-22 11:27:56 +0300

    done with homepage design

--- justus.js
+++ justus.js
@@ -3,18 +3,19 @@
        $('.parallax').parallax();
        $(".button-collapse").sideNav();
        $('select').material_select();
-       
+
        $(".dropdown-button").dropdown();
+       
 
        $('.datepicker').pickadate({
 		    selectMonths: true, 
 		    selectYears: 15 
 		  });
 
-       $(document).ready(function(){
+     
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
-  });
+  
 
 
     });
